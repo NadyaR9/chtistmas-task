3bcf0cb62458b93ddfe23bb6088839d71b6b5ded
Nadya <snotra.notra@gmail.com>
2022-01-17 11:48:10 +0300

    feat: upload build

--- src/pages/Firs/index.ts
+++
@@ -1,1 +0,0 @@
-export { Firs } from './Firs';
--- src/utils/getResource.ts
+++
@@ -1,8 +0,0 @@
-async function getResource(url: string) {
-  const res: Response = await fetch(url);
-  if (!res.ok) {
-    throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-  }
-  return await res.json();
-}
-export default getResource;
